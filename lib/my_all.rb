b88ca4c82ba9e75c54953fd59accf190741a7dca
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-15 18:57:02 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 1
+  block_retrun_values = []
+  while i < collection.length 
+    block_return_avlues << yield(collection[i])
+    i = i + 1 
 end
+
+if block_retrun_values.include?(false)
+    false
+  else
+    TRUE
+  end
+end
+
+my_all?([1,2,3]) {|i| i < 2 
